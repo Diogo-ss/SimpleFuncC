d360947cae307c416ec7b1a0bad5abaa0250ee91
Diogo-ss <diogosantosdasilva6@gmail.com>
2022-06-28 22:09:38 -0300

    str

---
+++ ft_concatenation_str.c
@@ -0,0 +1,45 @@
+#include <malloc.h>
+
+char *ft_concatenar(char *a, char *b)
+{
+    int ca;
+    int cb;
+    int cc;
+    int cl;
+    int cn;
+
+    char *pt;
+
+    ca = 0;
+    cb = 0;
+    cl = 0;
+    cn = 0;
+
+    while (a[ca]) {
+        ca++;
+    }
+
+    while (b[cb]) {
+        cb++;
+    }
+
+    cc = ca + cb;
+ 
+    pt = (char *) malloc((cc + 1) * sizeof(char));
+
+    while (cl < ca) 
+    {
+        pt[cl] = a[cl];
+        cl++;
+    }
+
+    while (cn < cb) 
+    {
+        pt[cl] = b[cn];
+
+        cl++;
+        cn++;
+    }
+
+    return pt;
+}
